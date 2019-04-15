962f40cdf18bf712dfde0a8f153ed48fe00a8e0b
skylert7 <skyler.linhtran@gmail.com>
2019-04-15 11:45:58 -0500

    Start

---
+++ src/ProfilePage/ProfilePage.js
@@ -0,0 +1,11 @@
+// Import ReactDOM and React
+import React from 'react';
+
+
+export default class ProfilePage extends React.Component {
+  render() {
+    return(
+      <h1 style = {{background-color:blue}}>'This is Profile Page' </h1>
+    );
+  }
+}
