7016a9bea549f3e515425759ca649e675889878a
skylert7 <skyler.linhtran@gmail.com>
2019-09-13 01:53:16 -0500

    works now

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var path = require('path');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var app = express();
+
+process.env.SECRET_KEY = 'secret'
+const port = process.env.PORT || 4000;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+var sess = {
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true,
+	cookie  : { maxAge  :  86400 * 1000}
+}
+app.use(express.static(__dirname + '/public'));
+
+//------------------------------------------------------------------------ Homepage -----------------------------------------------------------------------
+
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, './public', 'index.html'));
+});
+
+//---------------------------------------------------------------------------------
+app.listen(port, () => {
+	console.log("Server is running on port: " + port)
+})
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// Import ReactDOM and React
+Import ReactDOM and React
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import Home from './Home/Home';
@@ -7,22 +7,22 @@ import Slither from './slither/Slither';
 import Sudoku from './sudoku/Sudoku';
 import Tetris from './tetris/Tetris';
 import Nav from './Navigation/Nav'
-// var faker = require('faker');
+var faker = require('faker');
 
 
 class App extends React.Component {
   render(){
     return (
       <div>
-        <BrowserRouter>
-          <div>
-              <Route path = "/" exact component={Home}/>
-              <Route path = '/bio' exact component={Bio}/>
-              <Route path = '/snake' exact component = {Slither}/>
-              <Route path = "/tetris" exact component={Tetris}/>
-              <Route path = "/sudoku" exact component={Sudoku}/>
-          </div>
-        </BrowserRouter>
+        // <BrowserRouter>
+        //   <div>
+        //       <Route path = "/" exact component={Home}/>
+        //       <Route path = '/bio' exact component={Bio}/>
+        //       <Route path = '/snake' exact component = {Slither}/>
+        //       <Route path = "/tetris" exact component={Tetris}/>
+        //       <Route path = "/sudoku" exact component={Sudoku}/>
+        //   </div>
+        // </BrowserRouter>
       </div>
     )
   }
