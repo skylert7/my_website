932b92a03bc401a377425e8a048619f0a1feda9b
skylert7 <skylert@smu.edu>
2019-10-17 20:33:04 -0500

    fix stack collapse

--- src/App.js
+++ src/App.js
@@ -12,17 +12,7 @@ import Nav from './Navigation/Nav'
 class App extends React.Component {
   render(){
     return (
-      <div>
-        // <BrowserRouter>
-        //   <div>
-        //       <Route path = "/" exact component={Home}/>
-        //       <Route path = '/bio' exact component={Bio}/>
-        //       <Route path = '/snake' exact component = {Slither}/>
-        //       <Route path = "/tetris" exact component={Tetris}/>
-        //       <Route path = "/sudoku" exact component={Sudoku}/>
-        //   </div>
-        // </BrowserRouter>
-      </div>
+      <Home/>
     )
   }
 }
